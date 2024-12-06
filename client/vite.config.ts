ed8388427eeaa7746aa56f8996af2c8e7870d9cf
Egor Podolskiy <eapodolskiy@avito.ru>
2024-12-07 01:08:02 +0300

    add docker containers

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-	plugins: [react()],
-	base: '/itmo-practice/',
-})
+  plugins: [react()],
+  base: '/itmo-practice/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4444',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+});
