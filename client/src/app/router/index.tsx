fd4846a6a97a53bfc22bdc66fa8c01dca95beaff
Egor Podolskiy <eapodolskiy@avito.ru>
2024-12-07 00:35:26 +0300

    fix routing for gh pages

--- client/src/app/router/index.tsx
+++ client/src/app/router/index.tsx
@@ -1,8 +1,7 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { AppRoutes } from '../../types/routes.ts';
 import { Layout } from './components/layout/index.tsx';
-import { GITHUB_PAGES_BASE_APP_ROUTER } from './constants/index.ts';
 
 const GlossaryPage = lazy(() => import('../../pages/glossary-page/index.tsx'));
 const GraphPage = lazy(() => import('../../pages/graph-page/index.tsx'));
@@ -10,7 +9,7 @@ const NotFoundPage = lazy(() => import('../../pages/not-found-page/index.tsx'));
 
 export const Router = () => {
 	return (
-		<BrowserRouter basename={GITHUB_PAGES_BASE_APP_ROUTER}>
+		<HashRouter>
 			<Suspense fallback={null}>
 				<Routes>
 					<Route path={AppRoutes.Glossary} element={<Layout />}>
@@ -20,6 +19,6 @@ export const Router = () => {
 					<Route path='*' element={<NotFoundPage />} />
 				</Routes>
 			</Suspense>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
--- client/src/components/graph/index.tsx
+++ client/src/components/graph/index.tsx
@@ -7,7 +7,6 @@ import {
 } from '@xyflow/react';
 import { fallbackEdges, fallbackNodes } from '../../constants/fallback-data';
 import '@xyflow/react/dist/style.css';
-import styles from './styles.module.css';
 
 interface GraphProps {
 	nodes?: Node[];
@@ -20,15 +19,13 @@ export const Graph = ({ nodes, edges, useMockData = false }: GraphProps) => {
 	const displayEdges = useMockData && !edges ? fallbackEdges : edges;
 
 	return (
-		<div className={styles.graphWrapper}>
-			<ReactFlow
-				nodes={displayNodes}
-				edges={displayEdges}
-				fitView
-			>
-				<Controls showInteractive={false} />
-				<Background />
-			</ReactFlow>
-		</div>
+		<ReactFlow
+			nodes={displayNodes}
+			edges={displayEdges}
+			fitView
+		>
+			<Controls showInteractive={false} />
+			<Background />
+		</ReactFlow>
 	);
 };
