37f185b2541e18a5eae6a2ce06f6e662254e879a
Egor Podolskiy <eapodolskiy@avito.ru>
2024-12-06 22:27:48 +0300

    fix file structure

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# Прекращать выполнение скрипта при ошибках
+set -e
+
+# Сборка проекта
+npm run build ./client
+
+# Переход в папку сборки
+cd dist
+
+# Инициализация Git в папке dist
+git init
+git add -A -f
+git commit -m 'deploy'
+
+git push -f git@github.com:egorchh/itmo-practice.git main:gh-pages
+
+# Возврат в корень проекта
+cd -
