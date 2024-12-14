ef90e1431e2cbee9037fcadcd2556b21eb14a13f
Egor Podolskiy <eapodolskiy@avito.ru>
2024-12-14 08:53:38 +0300

    upd system files

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env sh
 
-# Прекращать выполнение скрипта при ошибках
 set -e
 
-# Сборка проекта
 npm run build
 
-# Переход в папку сборки
 cd dist
 
-# Инициализация Git в папке dist
 git init
 git add -A -f
 git commit -m 'deploy'
 
 git push -f git@github.com:egorchh/itmo-practice.git main:gh-pages
 
-# Возврат в корень проекта
 cd -
